60f46e6edad88097591ded93dccd5bd099abc3b1
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-22 20:59:29 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class StringCompare {
+
+	public static void main(String[] args) {
+		
+		String str3 = new String("Kutty");
+		String str4 = new String("Kutty");			
+		if (str3 == str4) 			
+		{
+			System.out.println(" Same text");
+		} 
+		else
+			System.out.println("Diff text");
+
+		System.out.println("***");
+
+	}
+
+}
